7d135e533805442cc1fba6e0342cecd262cf6443
Ella Frost <ded_19@hotmail.com>
2022-05-15 21:07:20 +0200

    ny

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+
 
 public class Main
 {
@@ -7,16 +9,34 @@ public class Main
         Scanner sc = new Scanner(System.in);
         ArrayList<User> users = new ArrayList<User>();
         for(int i = 0 ; i < 5; i++) {
-            System.out.println("Type in name number " + (i+1) + " of 5");
+            //System.out.println("Type in name number " + (i+1) + " of 5");
             users.add(new User(sc.nextLine()));
         }
         users.get(0).setName("hans");
+
+
     }
 
 
     public static void main(String[] args)
     {
         Main prg = new Main();
-        prg.go();
+        //prg.go();
+        ArrayList<User> sortingNames = new ArrayList<User>();
+        User firstUser = new User("Bob",10);
+        User secondUser = new User("Ken",15);
+        User thirdUser = new User("Lena",120);
+        User fourthUser = new User("Karen",8);
+        User FifthUser = new User("Linda",65);
+        sortingNames.add(firstUser);
+        sortingNames.add(secondUser);
+        sortingNames.add(thirdUser);
+        sortingNames.add(fourthUser);
+        sortingNames.add(firstUser);
+
+        System.out.println(sortingNames);
+        Collections.sort(sortingNames);
+        System.out.println(sortingNames);
+
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,9 +1,16 @@
-public class User
+public class User implements Comparable<User>
 {
     private String name;
+    private int age;
 
     public User(String name){
         this.name = name;
+    }
+
+    public User(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name){
@@ -14,9 +21,29 @@ public class User
         return name;
 
     }
+    public int getAge(){
+        return age;
+    }
+
+    public void skitse(User other){
+        //This will output what????
+        this.name.compareTo(other.getName());
+    }
+
+    @Override
+    public int compareTo(User otherUser){
+        if(this.age== otherUser.getAge()) {
+            return 0;
+        }
+        else if (this.age<otherUser.getAge()) {
+            return -1;
+        }
+        else
+            return 1;
+    }
 
     @Override
     public String toString(){
-        return name;
+        return name + " " + age;
     }
 }
